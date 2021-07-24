d54a3cc3f4166f23b3ebec6a20f985994ddf32aa
Psaini9211 <86410039+Psaini9211@users.noreply.github.com>
2021-07-25 03:35:50 +0530

    initial cart js

--- cart.js
+++ cart.js
@@ -1,13 +1,18 @@
+
+//cart
+
 let cartn = document.getElementById("cartNo");
-let my = JSON.parse(localStorage.getItem("cartNo"));
+let my = JSON.parse(localStorage.getItem("cart"));
 if(my != null){
     cartn.innerHTML = my.length;
 }
 
+var alert1 = document.getElementById("alert1");
+var alert2 = document.getElementById("absolute")
 let j=0;
 
 function cartPage(){
-    let res=true;
+    let tempo = true;
     if(j==0){
         let myarr;
         if(localStorage.getItem("cart") == null){
@@ -15,7 +20,8 @@ function cartPage(){
             myarr.push(value);
             alert2.style.display="block";
             alert2.textContent ="";
-            alert2.append(child1,child2,child3);
+            alert2.textContent = "Item Succesfully Added to Cart"
+            tempo=false;
             setTimeout(function(){
                 alert2.style.display = "none";
             },5000);
@@ -25,30 +31,96 @@ function cartPage(){
         }
     
         for(let k=0 ; k<myarr.length ; k++){
-            if((myarr[k].name == value.name) && (myarr[k].image1 == value.image1)){
-                res = false;
+            if((myarr[k].name != value.name) && (myarr[k].image1 != value.image1)){
+                myarr.push(value);
+                tempo = false;
+                alert2.style.display="block";
+                alert2.textContent ="";
+                alert2.textContent = "Item Succesfully Added to Cart"
+                setTimeout(function(){
+                    alert2.style.display = "none";
+                },5000);
                 break;
             }
-        }
-
-        if(res == true){
-            console.log(res)
-            myarr.push(value);
-            alert2.style.display="block";
-            alert2.textContent ="";
-            alert2.append(child1,child2,child3);
-            setTimeout(function(){
-                alert2.style.display = "none";
-            },5000);
         }
         localStorage.setItem("cart",JSON.stringify(myarr));
         cartn.innerHTML = myarr.length;
     }
     j++;
-    if(res == false){
+    if(tempo){
         alert1.innerHTML="";
         alert1.innerHTML= "Sorry! You can only add the displayed number of items to the cart. Please enter quantity up to 1.";
     }
 }
 
+function displayCart() {
+    let cartn = localStorage.getItem("cart")
+    cartn = JSON.parse(cartn)
+    
+    let productContainer = document.querySelector
+        (".product-details");
+    
+    console.log(cartn);
+    if (cartn && productContainer ) {
+        productContainer.innerHTML = '';
+        Object.values(cartn).map(item => {
+            productContainer.innerHTML += `
+            <img src="${item.image1}">
+					</div>
+					<div class="product-dis">
+						<p>${item.name}</p>
+						<p id="product-urn">54681PPVVAAA02EA000070</p>
+						<p style="margin-top: -10px;"> &#x20b9; ${item.price}</p>
+						<p id="weight">Weight (g): ${item.weight}</p>
+						<div>
+							<button type="button" data-toggle="dropdown" class="wishlist-btn">
+								<span style="float: left;">Move to Wishlist</span>
+							</button>
+						</div>
+					</div>
+					<div class="product-quantity">
+                        <div>
+							<fieldset class= "fld">-</fieldset>
+							<input type="text" class="input-quntity" value="1" maxlength="2" minlength="1">
+							<fieldset  class= "fld">+</fieldset>
+						</div>
+						<button type="submit" id="remove-product">
+							REMOVE
+						</button>
+						<div class="checkbox">
+						<input id="input" type="checkbox" style="height: 20px; width: 20px;" >
+						<label class="label" style="margin-top: -30px;">Add a gift message</label>
+						</div>
+					</div>
+					<span id="product_price">
+						<p> &#x20b9; ${item.price}</p>
+					</span>
+				</div>
+				<hr style="margin-top: 300px;">
+				<div class="shipping-container">
+					<div style="float: left;">
+					<p>SHIPPING METHOD:</p>
+					<button type="submit" class="home-delivery">HOME DELIVERY</button>
+					</div>
+					<div class="contry">
+						<p>COUNTRY</p>
+						<select>
+							<option>INDIA</option>
+							<option>AMRICA</option>
+						</select>
+					</div>
+					<div class="pin">
+						<p style="color: gray;">Pin-Code/Zip-Code</p>
+						<input type="text">
+						<button>CHECK</button>
+					</div>
+				</div>
+				<hr style="margin-top: 150px;">
+            `
+        });
+           
+        
+    }
+}
 
+displayCart();
